a860604175fd0c413d9ccc7c0b5ecec1fae493d8
Ax-LED <37046881+Ax-LED@users.noreply.github.com>
2018-12-27 18:08:28 +0100

    Add files via upload

--- MMM-Dreambox.js
+++ MMM-Dreambox.js
@@ -17,22 +17,17 @@ Module.register('MMM-Dreambox', {
 	
 	defaults: {
 		units: config.units,
-		//animationSpeed: 1000,
-		//refreshInterval: 1000 * 60, //refresh every minute
 		refreshInterval: 1000 * 30, //refresh every 15 seconds
 		timeFormat: config.timeFormat,
 		lang: config.language,
-
-		//initialLoadDelay: 0, // 0 seconds delay
-		//retryDelay: 2500,
-
 		apiBase: 'http://dm800se.fritz.box',//AxLED
 		apiabout: '/web/about',
 		apiservices: '/web/getallservices',
 		apiepgnow: '/web/epgnow?bRef=1%3A7%3A1%3A0%3A0%3A0%3A0%3A0%3A0%3A0%3AFROM%20BOUQUET%20%22userbouquet.favourites.tv%22%20ORDER%20BY%20bouquet',//'/web/epgnow?bRef=1%3A7%3A1%3A0%3A0%3A0%3A0%3A0%3A0%3A0%3A',
 		apizap: '/web/zap?sRef=',
 		apiTimerlist: '/web/timerlist',
-		apiServicelistplayable: '/web/servicelistplayable?sRef=1%3A7%3A1%3A0%3A0%3A0%3A0%3A0%3A0%3A0%3AFROM%20BOUQUET%20%22userbouquet.favourites.tv%22%20ORDER%20BY%20bouquet'
+		apiServicelistplayable: '/web/servicelistplayable?sRef=1%3A7%3A1%3A0%3A0%3A0%3A0%3A0%3A0%3A0%3AFROM%20BOUQUET%20%22userbouquet.favourites.tv%22%20ORDER%20BY%20bouquet',
+		omxargs: ' --win 320,180,1600,900  -o both '
 	},
 	
 	// Define required scripts.
@@ -45,7 +40,6 @@ Module.register('MMM-Dreambox', {
 	},
 
 	start: function() {
-		//var dataRequest = null;
 		Log.info('Starting module: ' + this.name);
 		this.loaded = false;
 		this.sendSocketNotification('CONFIG', this.config);
@@ -59,14 +53,13 @@ Module.register('MMM-Dreambox', {
 
 		//First Line
 		var ServiceItem2 = document.createElement("div");
-		//ServiceItem2.setAttribute('class', 'xxxMMM-Dreambox');
 		ServiceItem2.setAttribute('class', 'float');
 		if (this.model != undefined) {
 			ServiceItem2.innerHTML = 'Modell: ' +this.model+ '&nbsp;';
 		} else if (this.Errormessage != undefined){
-			ServiceItem2.innerHTML = 'Fehler: ' +this.Errormessage;
+			ServiceItem2.innerHTML = this.Errormessage;
 		}
-		//AXLED NEU
+		
 		var ServiceItem3 = document.createElement("div");
 		ServiceItem3.setAttribute('class', 'float');
 		if (this.timerstring != undefined){
@@ -74,22 +67,16 @@ Module.register('MMM-Dreambox', {
 		} else {
 			ServiceItem3.innerHTML = '- dezeit läuft: (' +this.tuned + ')';
 		}
-		//AXLED NEU
 
-		//add separator
 		var Separator = document.createElement("hr");
 		Separator.setAttribute('class', 'db');
-	//ServiceItem2.appendChild(Separator);
 
 		wrapper.appendChild(ServiceItem2);
-		//Axled Neu
-		//ServiceItem3.appendChild(Separator);
 		wrapper.appendChild(ServiceItem3);
 		wrapper.appendChild(Separator);
-		//Axled Neu
 
-		//Line for stations
-		if (this.dataRequest === "DB-SERVICES" || this.dataRequest === "DB-EPGNOW" || this.dataRequest === "DB-TIMER" || this.dataRequest === "DB-SLP") {//Axled kann diese Zeile evtl raus, da UpdateDOM immer funktionieren soll
+		//Divs for stations
+		if (this.dataRequest === "DB-SERVICES" || this.dataRequest === "DB-EPGNOW" || this.dataRequest === "DB-TIMER" || this.dataRequest === "DB-SLP") {//Axled can this part of code be optimized, because updateDom() should work anyway
 			for (let index = 0; index < this.anzahl; index++) {
 				var ServiceItem = document.createElement("div");
 				ServiceItem.setAttribute('class', 'db');
@@ -97,8 +84,6 @@ Module.register('MMM-Dreambox', {
 				if(this.epgtitle == undefined){
 					ServiceItem.innerHTML = this.sender[index].e2servicename;
 				} else {
-					//ServiceItem.innerHTML = this.sender[index].e2servicename +' - ('+this.epg(this.epgtitle,this.sender[index].e2servicereference)+')';
-					//ServiceItem.innerHTML = this.sender[index].e2servicename +' - ('+moment.unix(this.epgtitle[index].e2eventstart).format('HH:mm')+' '+this.epg(this.epgtitle,this.sender[index].e2servicereference)+')';
 					ServiceItem.innerHTML = this.sender[index].e2servicename +' - ('+moment.unix(this.epgtitle[index].e2eventstart).format('HH:mm')+' '+this.epgtitle[index].e2eventtitle+')';
 				}
 				
@@ -106,13 +91,11 @@ Module.register('MMM-Dreambox', {
 
 				if(this.tuned === this.sender[index].e2servicename) {
 					ServiceItem.setAttribute('class', 'selected');
-					serviceselected = index;// remember the selected service for zapping
+					serviceselected = index;// remember the selected service for play & zapping
 				}
 				//mark playable services (only if timer is running)
 				if(this.slp != undefined && this.timerstring != undefined){
-				//if(this.slp != undefined){
 					if(this.slp[index].e2isplayable === "False"){
-						//console.log('AxLED slp erreicht:',this.slp[index].e2isplayable);
 						ServiceItem.setAttribute('class', 'inactive');
 						onlyplayable = true;
 					}
@@ -120,38 +103,28 @@ Module.register('MMM-Dreambox', {
 				wrapper.appendChild(ServiceItem);
 			}
 		} 
-		
 		return wrapper;
 	},
 
 	nextselection: function (selected, direction) {
-		//console.log('Axled onlyplayable in function nextselection:',onlyplayable);
 		if (onlyplayable === true){
 			serviceselected = selected;
 			newserviceselected = '';
 			var x = document.querySelectorAll('div.db, div.selected');
-			//console.log('AxLED Serviceselected:',serviceselected);
 			
 			if (direction === '+'){
 				if (serviceselected == ''){
 					serviceselected = 0;
 				} else {
-					//remove old selection
-					document.getElementById(serviceselected).setAttribute('class','db');
+					document.getElementById(serviceselected).setAttribute('class','db');//remove old selection
 				}
 				
-				//var x = document.querySelectorAll('div.db, div.selected');
 				for (let index = 0; index < x.length; index++) {
-					//console.log('Axled function nextselection:',x[index].id);
 					if (serviceselected < x[index].id && newserviceselected == '') {
 						newserviceselected = x[index].id;
-						//console.log('Axled plus serviceselected/newserviceselected:',serviceselected,'/',newserviceselected);
-						//set new selection
-						document.getElementById(newserviceselected).setAttribute('class','selected');
+						document.getElementById(newserviceselected).setAttribute('class','selected');//set new selection
 					} else if (serviceselected == x[x.length-1].id) {//if last entry is reached, set the first value for looping through the playable stattions
-						//newserviceselected = x[index].id;
 						newserviceselected = x[0].id;
-						//console.log('Axled else erreicht:',serviceselected,'/',newserviceselected);
 						document.getElementById(newserviceselected).setAttribute('class','selected');
 					}
 				}
@@ -159,22 +132,15 @@ Module.register('MMM-Dreambox', {
 				if (serviceselected == ''){
 					serviceselected = this.anzahl-1;
 				} else {
-					//remove old selection
-					document.getElementById(serviceselected).setAttribute('class','db');
+					document.getElementById(serviceselected).setAttribute('class','db');//remove old selection
 				}
-				//console.log('Axled minus direction erreicht.');
-				//for (let index = 0; index < x.length; index++) {
+
 				for (let index = x.length-1; index >= 0; index--) {//negative loop
-					//console.log('Axled negative for schleife/serviceselected/newserviceselected:',x[index].id,'/',serviceselected,'/', newserviceselected);
 					if (serviceselected > x[index].id && newserviceselected == '') {
 						newserviceselected = x[index].id;
-						//console.log('Axled minus serviceselected/newserviceselected:',serviceselected,'/',newserviceselected);
-						//set new selection
-						document.getElementById(newserviceselected).setAttribute('class','selected');
+						document.getElementById(newserviceselected).setAttribute('class','selected');//set new selection
 					} else if (serviceselected == x[0].id) {//if first entry is reached, set the last value for looping through the playable stattions
-						//newserviceselected = x[index].id;
 						newserviceselected = x[x.length-1].id;
-						//console.log('Axled else erreicht:',serviceselected,'/',newserviceselected);
 						document.getElementById(newserviceselected).setAttribute('class','selected');
 					}
 				}
@@ -182,42 +148,27 @@ Module.register('MMM-Dreambox', {
 		} else { //onlyplayable = false or ''
 			serviceselected = selected;
 			newserviceselected = '';
-			if (direction === '+'){// Next
-				//console.log('1. Axled serviceselected/newserviceselected:',serviceselected,'/',newserviceselected);
-				//if (serviceselected === ''){
+			if (direction === '+'){
 				if (serviceselected === '' || serviceselected === this.anzahl-1){
 					serviceselected = -1;
 					document.getElementById(parseInt(this.anzahl-1)).setAttribute('class','db');
-				/*} else if (serviceselected === this.anzahl-1){
-					serviceselected = -1;*/
 				} else {
-					//remove old selection
-					document.getElementById(serviceselected).setAttribute('class','db');
+					document.getElementById(serviceselected).setAttribute('class','db');//remove old selection
 				}
-				//console.log('2. Axled serviceselected/newserviceselected:',serviceselected,'/',newserviceselected);
 				document.getElementById(parseInt(serviceselected+1)).setAttribute('class','selected');
 				newserviceselected = parseInt(serviceselected+1);
-				//console.log('3. Axled serviceselected/newserviceselected:',serviceselected,'/',newserviceselected);
-			} else if (direction === '-'){// Prev
-				//console.log('1. Axled serviceselected/newserviceselected:',serviceselected,'/',newserviceselected);
-				//if (serviceselected === 0){
-				//serviceselected = parseInt(this.anzahl)-1;
+			} else if (direction === '-'){
 				if (serviceselected === '' || serviceselected === 0){
-					//serviceselected = parseInt(this.anzahl)-1;
 					serviceselected = parseInt(this.anzahl);
 					document.getElementById(0).setAttribute('class','db');
 				} else {
-					//remove old selection
-					document.getElementById(serviceselected).setAttribute('class','db');
+					document.getElementById(serviceselected).setAttribute('class','db');//remove old selection
 				}
-				//console.log('2. Axled serviceselected/newserviceselected:',serviceselected,'/',newserviceselected);
 				document.getElementById(parseInt(serviceselected-1)).setAttribute('class','selected');
 				newserviceselected = parseInt(serviceselected-1);
-				//console.log('3. Axled serviceselected/newserviceselected:',serviceselected,'/',newserviceselected);
 			}
 		}
 		return newserviceselected;
-		//return;
 	},
 	
 	//Helper, to use module with notification system
@@ -232,13 +183,11 @@ Module.register('MMM-Dreambox', {
 
 		if(notification === "DB-PLAY"){
 			document.getElementById(serviceselected).setAttribute('class','selected play');
-			if (onlyplayable === true){
-				//add zap information
+			if (onlyplayable === true){//add zap information
 				payload = [this.sender[parseInt(serviceselected)].e2servicereference,''];
 			} else {
 				payload = [this.sender[parseInt(serviceselected)].e2servicereference,'zap'];
 			}
-			//console.log('Axled DB-PLAY1:',payload);
 			this.sendSocketNotification('DB-PLAY', payload);
 		}
 		
@@ -249,12 +198,7 @@ Module.register('MMM-Dreambox', {
 	
  	socketNotificationReceived: function(notification, payload) {
     		if (notification === "STARTED") {
-				//Neu
 				this.startFetchingData(this.config.refreshInterval);
-				//Neu
-				//ALT
-				//this.updateDom();
-				//ALT
 			} else if (notification === "DATA") {
 				this.loaded = true;
 				if(payload[0]==='DB-EPGNOW'){
@@ -281,16 +225,13 @@ Module.register('MMM-Dreambox', {
 				} else if(payload[0]==='DB-TIMER'){
 					this.dataRequest = payload[0];
 					var json=xml2json(payload[1]);
-					//console.log('Axled DB-TIMER',json);
-					if (json.e2timerlist.e2timer[0].e2state === "2"){//Timer in List are sorted, so only first entry can run
+					if (json.e2timerlist.e2timer[0].e2state === "2"){//Timer in List are sorted, so only first entry can (should) run, sometimes the second entry runs
 						this.timerstring = 'Aufnahme läuft: '+json.e2timerlist.e2timer[0].e2name+' ('+json.e2timerlist.e2timer[0].e2servicename+')';
-						//console.log('Axled Timer läuft: ',this.timerstring);
 						this.updateDom();
 					} 
 				} else if(payload[0]==='DB-SLP'){
 					this.dataRequest = payload[0];
 					var json=xml2json(payload[1]);
-					//console.log('Axled DB-SLP',json);
 					this.slp = json.e2servicelistplayable.e2serviceplayable;
 					this.updateDom();
 				} 
@@ -299,17 +240,11 @@ Module.register('MMM-Dreambox', {
 	},
 	
 	startFetchingData: function(interval) {
-		// start immediately ...
-		//this.sendSocketNotification("FETCH_DATA", '');
 		if (IntervalID === ''){
 			// ... and then repeat in the given interval
-			//setInterval(() => {
 			IntervalID = setInterval(() => {
-			//setTimeout(() => {
 			this.sendSocketNotification("FETCH_DATA", '');
-			//console.log(moment().format('LTS'),': Axled Interval ausgelöst.');
 			}, interval);
-			//console.log('Axled Intervall ID:',IntervalID);
 		}
 	}
 
--- node_helper.js
+++ node_helper.js
@@ -8,10 +8,9 @@
  */
 
 const NodeHelper = require('node_helper');
-//var request = require('request');
 const request = require('request');
 const exec = require("child_process").exec;
-//var moment = require('moment');
+
 var body_about = '';
 var body_services = '';
 var body_epgnow = '';
@@ -22,7 +21,6 @@ module.exports = NodeHelper.create({
 	start: function() {
 		this.started = false;
 		this.config = null;
-		//console.log('Axled Startfunction node_helper');
 	},
 
 	socketNotificationReceived: function(notification, payload) {
@@ -31,51 +29,31 @@ module.exports = NodeHelper.create({
 			self.config = payload;
 			self.sendSocketNotification("STARTED", true);
 			self.getData2();//Inittal Dataload before timer interval is activated
-			//self.started = true; //AxKED: Wenn die Zeile nicht auskommentiert ist, dann geht im Browser F5 refresh nicht..
+			//self.started = true; //AxLED: if this line is active, Browserrefresh F5 does not work
 			console.log("Starting node helper for: " + self.name);
 		} 
 
-		/*if (notification === "DB-PLAY") {
-			var myUrl = this.config.apiBase;
-			request({url: myUrl+this.config.apizap+payload }, function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					//http://dm800se.fritz.box:8001/1:0:1:D176:2718:F001:FFFF0000:0:0:0:
-					//omx.play("modules/MMM-Podcast/video.mp4");
-					//exec('omxplayer --live --win 320,180,1600,900 -o hdmi '+self.config.apiBase+':8001/'+payload, null);
-					//exec('curl http://dm800se.fritz.box/web/remotecontrol?command=352',null);//send remote control Button "OK", because of question while zapping when somebody watches recorded movies meanwhile
-					exec('omxplayer --win 320,180,1600,900 -o hdmi '+self.config.apiBase+':8001/'+payload, null);//without --live buffering works
-					self.getData2();
-				}
-			});
-		}*/
-
 		if (notification === "DB-PLAY") {
 			if (payload[1] === 'zap') {
-				//console.log('Axled payload enthält zap');
 				var myUrl = this.config.apiBase;
 				request({url: myUrl+this.config.apizap+payload[0] }, function (error, response, body) {
 					if (!error && response.statusCode == 200) {
-						//http://dm800se.fritz.box:8001/1:0:1:D176:2718:F001:FFFF0000:0:0:0:
-						//omx.play("modules/MMM-Podcast/video.mp4");
-						//exec('omxplayer --live --win 320,180,1600,900 -o hdmi '+self.config.apiBase+':8001/'+payload, null);
-						//exec('curl http://dm800se.fritz.box/web/remotecontrol?command=352',null);//send remote control Button "OK", because of question while zapping when somebody watches recorded movies meanwhile
-						exec('omxplayer --win 320,180,1600,900 -o both '+self.config.apiBase+':8001/'+payload[0], null);//without --live buffering works
+						//exec('omxplayer --win 320,180,1600,900 -o both '+self.config.apiBase+':8001/'+payload[0], null);//without --live buffering works
+						exec('omxplayer '+self.config.omxargs+self.config.apiBase+':8001/'+payload[0], null);//without '--live' buffering works
 					}
 				});
 			} else {
-				//console.log('Axled payload enthält kein zap');
-				exec('omxplayer --win 320,180,1600,900 -o both '+self.config.apiBase+':8001/'+payload[0], null);//without --live buffering works
+				//exec('omxplayer --win 320,180,1600,900 -o both '+self.config.apiBase+':8001/'+payload[0], null);//without '--live' buffering works
+				exec('omxplayer '+self.config.omxargs+self.config.apiBase+':8001/'+payload[0], null);//without --live buffering works
 			}
 			self.getData2();
 		}
 
 		if (notification === "DB-STOP") {
-			//console.log('Axled: test');
 			exec('pkill omxplayer', null);
 		}
 
 		if (notification === "FETCH_DATA") {
-			//console.log('AxLed getdata2 aufgerufen.');
 			self.getData2();
 		}
 	},
@@ -91,7 +69,7 @@ module.exports = NodeHelper.create({
 				body_epgnow = body;
 				self.sendSocketNotification("DATA",['DB-EPGNOW',body_epgnow]);
 			} else {
-				Errormessage = 'MMM-Dreambox Error '+error.code+' in '+myUrl+self.config.apiepgnow;
+				Errormessage = 'Error: '+error.code+' in '+myUrl+self.config.apiepgnow;
 				self.sendSocketNotification("DATA",['ERROR',Errormessage]);
 			}
 		});
@@ -101,7 +79,7 @@ module.exports = NodeHelper.create({
 				body_about = body;
 				self.sendSocketNotification("DATA",['DB-ABOUT',body_about]);
 			} else {
-				Errormessage = 'MMM-Dreambox Error '+error.code+' in '+myUrl+self.config.apiabout;
+				Errormessage = 'Error: '+error.code+' in '+myUrl+self.config.apiabout;
 				self.sendSocketNotification("DATA",['ERROR',Errormessage]);
 			}
 		});
@@ -111,17 +89,16 @@ module.exports = NodeHelper.create({
 				body_services = body;
 				self.sendSocketNotification("DATA",['DB-SERVICES',body_services]);
 			} else {
-				Errormessage = 'MMM-Dreambox Error '+error.code+' in '+myUrl+self.config.apiservices;
+				Errormessage = 'Error: '+error.code+' in '+myUrl+self.config.apiservices;
 				self.sendSocketNotification("DATA",['ERROR',Errormessage]);
 			}
 		});
 		//4.Request - Timerlist
 		request({url: myUrl+this.config.apiTimerlist }, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
-				//body_services = body;
 				self.sendSocketNotification("DATA",['DB-TIMER',body]);
 			} else {
-				Errormessage = 'MMM-Dreambox Error '+error.code+' in '+myUrl+self.config.apiservices;
+				Errormessage = 'Error: '+error.code+' in '+myUrl+self.config.apiservices;
 				self.sendSocketNotification("DATA",['ERROR',Errormessage]);
 			}
 		});
@@ -130,7 +107,7 @@ module.exports = NodeHelper.create({
 			if (!error && response.statusCode == 200) {
 				self.sendSocketNotification("DATA",['DB-SLP',body]);
 			} else {
-				Errormessage = 'MMM-Dreambox Error '+error.code+' in '+myUrl+self.config.apiservices;
+				Errormessage = 'Error: '+error.code+' in '+myUrl+self.config.apiservices;
 				self.sendSocketNotification("DATA",['ERROR',Errormessage]);
 			}
 		});
