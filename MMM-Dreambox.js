62f8ffdc99cfef94633c17cb8b01991fb42521b0
Ax-LED <37046881+Ax-LED@users.noreply.github.com>
2019-01-04 14:07:07 +0100

    Code optimization to use less ressources

--- MMM-Dreambox.js
+++ MMM-Dreambox.js
@@ -17,13 +17,13 @@ Module.register('MMM-Dreambox', {
 	
 	defaults: {
 		units: config.units,
-		refreshInterval: 1000 * 30, //refresh every 15 seconds
+		refreshInterval: 1000 * 30, //refresh every 30 seconds
 		timeFormat: config.timeFormat,
 		lang: config.language,
-		apiBase: 'http://dm800se.fritz.box',//AxLED
+		apiBase: 'http://ipofyourreceiver:port',//AxLED
 		apiabout: '/web/about',
 		apiservices: '/web/getallservices',
-		apiepgnow: '/web/epgnow?bRef=1%3A7%3A1%3A0%3A0%3A0%3A0%3A0%3A0%3A0%3AFROM%20BOUQUET%20%22userbouquet.favourites.tv%22%20ORDER%20BY%20bouquet',//'/web/epgnow?bRef=1%3A7%3A1%3A0%3A0%3A0%3A0%3A0%3A0%3A0%3A',
+		apiepgnow: '/web/epgnow?bRef=1%3A7%3A1%3A0%3A0%3A0%3A0%3A0%3A0%3A0%3AFROM%20BOUQUET%20%22userbouquet.favourites.tv%22%20ORDER%20BY%20bouquet',
 		apizap: '/web/zap?sRef=',
 		apiTimerlist: '/web/timerlist',
 		apiServicelistplayable: '/web/servicelistplayable?sRef=1%3A7%3A1%3A0%3A0%3A0%3A0%3A0%3A0%3A0%3AFROM%20BOUQUET%20%22userbouquet.favourites.tv%22%20ORDER%20BY%20bouquet',
@@ -58,79 +58,38 @@ Module.register('MMM-Dreambox', {
 
 		// create element wrapper for show into the module
 		var wrapper = document.createElement("div");
+		wrapper.setAttribute('id', 'MMM-Dreambox');
 
-		//First Line
 		//Errorhandling
-		if (this.Errormessage != undefined){
-			var ServiceItem4 = document.createElement("div");
-			ServiceItem4.setAttribute('class', 'float');
-			ServiceItem4.innerHTML = this.Errormessage;
-			var Separator = document.createElement("hr");
-			Separator.setAttribute('class', 'db');
-			wrapper.appendChild(ServiceItem4);
-			wrapper.appendChild(Separator);
-		}
+		var Errorinfo = document.createElement("div");
+		Errorinfo.setAttribute('class', 'float');
+		Errorinfo.setAttribute('id','error1');
+		Errorinfo.setAttribute('style','display: none;');
+		Errorinfo.innerHTML = '';
+		var Separator = document.createElement("hr");
+		Separator.setAttribute('class', 'db');
+		Separator.setAttribute('id','error2');
+		Separator.setAttribute('style','display: none;');
+		wrapper.appendChild(Errorinfo);
+		wrapper.appendChild(Separator);
 		
-		var ServiceItem2 = document.createElement("div");
-		ServiceItem2.setAttribute('class', 'float');
-		if (this.model != undefined) {
-			//ServiceItem2.innerHTML = 'Modell: ' +this.model+ '&nbsp;';
-			//this.translate("INFO")
-			ServiceItem2.innerHTML = this.translate("model") +this.model+ '&nbsp;';
-		} /*else if (this.Errormessage != undefined){
-			ServiceItem2.innerHTML = this.Errormessage;
-		}*/
+		//Model of Receiver
+		var Receiverinfo = document.createElement("div");
+		Receiverinfo.setAttribute('class', 'float');
+		Receiverinfo.setAttribute('id','model');
+		Receiverinfo.innerHTML = '';
 		
-		var ServiceItem3 = document.createElement("div");
-		ServiceItem3.setAttribute('class', 'float');
-		if (this.timerstring != undefined){
-			ServiceItem3.innerHTML = '- '+ this.timerstring;
-		} else {
-			//ServiceItem3.innerHTML = '- derzeit läuft: (' +this.tuned + ')';
-			ServiceItem3.innerHTML = '- ' +this.translate("nowplaying")+ '(' +this.tuned + ')';
-		}
-
+		//Info now playing
+		var Nowplayinginfo = document.createElement("div");
+		Nowplayinginfo.setAttribute('class', 'float');
+		Nowplayinginfo.setAttribute('id','nowplaying');
+		Nowplayinginfo.innerHTML = '';
 		var Separator = document.createElement("hr");
 		Separator.setAttribute('class', 'db');
 
-		wrapper.appendChild(ServiceItem2);
-		wrapper.appendChild(ServiceItem3);
+		wrapper.appendChild(Receiverinfo);
+		wrapper.appendChild(Nowplayinginfo);
 		wrapper.appendChild(Separator);
-
-		//Divs for stations
-		//if (this.dataRequest === "DB-SERVICES" || this.dataRequest === "DB-EPGNOW" || this.dataRequest === "DB-TIMER" || this.dataRequest === "DB-SLP") {//Axled can this part of code be optimized, because updateDom() should work anyway
-			for (let index = 0; index < this.anzahl; index++) {
-				var ServiceItem = document.createElement("div");
-				ServiceItem.setAttribute('class', 'db');
-				
-				if(this.epg == undefined){
-					ServiceItem.innerHTML = this.sender[index].e2servicename;
-				} else {
-					//console.log('Axled epg:',this.epg[index].e2eventstart);
-					if (this.epg[index].e2eventstart === "None") {//no epg information for this service
-						ServiceItem.innerHTML = this.sender[index].e2servicename+' - ('+this.translate("noepginformation")+')';
-					} else {
-						ServiceItem.innerHTML = this.sender[index].e2servicename +' - ('+moment.unix(this.epg[index].e2eventstart).format('HH:mm')+' '+this.epg[index].e2eventtitle+')';
-					}
-					//ServiceItem.innerHTML = this.sender[index].e2servicename +' - ('+moment.unix(this.epg[index].e2eventstart).format('HH:mm')+' '+this.epg[index].e2eventtitle+')';
-				}
-				
-				ServiceItem.setAttribute('id', index);
-
-				if(this.tuned === this.sender[index].e2servicename) {
-					ServiceItem.setAttribute('class', 'selected');
-					serviceselected = index;// remember the selected service for play & zapping
-				}
-				//mark playable services (only if timer is running)
-				if(this.slp != undefined && this.timerstring != undefined){
-					if(this.slp[index].e2isplayable === "False"){
-						ServiceItem.setAttribute('class', 'inactive');
-						onlyplayable = true;
-					}
-				}
-				wrapper.appendChild(ServiceItem);
-			}
-		//} 
 		return wrapper;
 	},
 
@@ -144,32 +103,36 @@ Module.register('MMM-Dreambox', {
 				if (serviceselected === ''){
 					serviceselected = 0;
 				} else {
-					document.getElementById(serviceselected).setAttribute('class','db');//remove old selection
+					document.getElementById('MMM-Dreambox'+serviceselected).setAttribute('class','db');//remove old selection
 				}
 				
 				for (let index = 0; index < x.length; index++) {
-					if (serviceselected < x[index].id && newserviceselected == '') {
-						newserviceselected = x[index].id;
-						document.getElementById(newserviceselected).setAttribute('class','selected');//set new selection
-					} else if (serviceselected == x[x.length-1].id) {//if last entry is reached, set the first value for looping through the playable stattions
-						newserviceselected = x[0].id;
-						document.getElementById(newserviceselected).setAttribute('class','selected');
+					idonly = x[index].id.substring (12, x[index].id.length);//exctract ID (only number) from ID-String by cutting the firstr 12 characters
+					idonly2 = x[x.length-1].id.substring (12, x[x.length-1].id.length);
+					if (serviceselected < idonly && newserviceselected == '') {
+						newserviceselected = idonly;
+						document.getElementById('MMM-Dreambox'+newserviceselected).setAttribute('class','selected');//set new selection
+					} else if (serviceselected == idonly2) {//if last entry is reached, set the first value for looping through the playable stattions
+						newserviceselected = x[0].id.substring (12, x[0].id.length);
+						document.getElementById('MMM-Dreambox'+newserviceselected).setAttribute('class','selected');
 					}
 				}
 			} else if (direction == '-'){
 				if (serviceselected === ''){
 					serviceselected = this.anzahl-1;
 				} else {
-					document.getElementById(serviceselected).setAttribute('class','db');//remove old selection
+					document.getElementById('MMM-Dreambox'+serviceselected).setAttribute('class','db');//remove old selection
 				}
 
 				for (let index = x.length-1; index >= 0; index--) {//negative loop
-					if (serviceselected > x[index].id && newserviceselected == '') {
-						newserviceselected = x[index].id;
-						document.getElementById(newserviceselected).setAttribute('class','selected');//set new selection
-					} else if (serviceselected == x[0].id) {//if first entry is reached, set the last value for looping through the playable stattions
-						newserviceselected = x[x.length-1].id;
-						document.getElementById(newserviceselected).setAttribute('class','selected');
+					idonly = x[index].id.substring (12, x[index].id.length);//exctract ID (only number) from ID-String by cutting the firstr 12 characters
+					idonly2 = x[0].id.substring (12, x[0].id.length);
+					if (serviceselected > idonly && newserviceselected == '') {
+						newserviceselected = idonly;
+						document.getElementById('MMM-Dreambox'+newserviceselected).setAttribute('class','selected');//set new selection
+					} else if (serviceselected == idonly2) {//if first entry is reached, set the last value for looping through the playable stattions
+						newserviceselected = x[x.length-1].id.substring (12, x[x.length-1].id.length);
+						document.getElementById('MMM-Dreambox'+newserviceselected).setAttribute('class','selected');
 					}
 				}
 			}
@@ -179,20 +142,20 @@ Module.register('MMM-Dreambox', {
 			if (direction === '+'){
 				if (serviceselected === '' || serviceselected === this.anzahl-1){
 					serviceselected = -1;
-					document.getElementById(parseInt(this.anzahl-1)).setAttribute('class','db');
+					document.getElementById('MMM-Dreambox'+parseInt(this.anzahl-1)).setAttribute('class','db');
 				} else {
-					document.getElementById(serviceselected).setAttribute('class','db');//remove old selection
+					document.getElementById('MMM-Dreambox'+serviceselected).setAttribute('class','db');//remove old selection
 				}
-				document.getElementById(parseInt(serviceselected+1)).setAttribute('class','selected');
+				document.getElementById('MMM-Dreambox'+parseInt(serviceselected+1)).setAttribute('class','selected');
 				newserviceselected = parseInt(serviceselected+1);
 			} else if (direction === '-'){
 				if (serviceselected === '' || serviceselected === 0){
 					serviceselected = parseInt(this.anzahl);
-					document.getElementById(0).setAttribute('class','db');
+					document.getElementById('MMM-Dreambox'+0).setAttribute('class','db');
 				} else {
-					document.getElementById(serviceselected).setAttribute('class','db');//remove old selection
+					document.getElementById('MMM-Dreambox'+serviceselected).setAttribute('class','db');//remove old selection
 				}
-				document.getElementById(parseInt(serviceselected-1)).setAttribute('class','selected');
+				document.getElementById('MMM-Dreambox'+parseInt(serviceselected-1)).setAttribute('class','selected');
 				newserviceselected = parseInt(serviceselected-1);
 			}
 		}
@@ -210,9 +173,7 @@ Module.register('MMM-Dreambox', {
 		}
 
 		if(notification === "DB-PLAY"){
-			document.getElementById(serviceselected).setAttribute('class','selected play');
-			//test if service is playable, if yes, no zap required 
-			//if (onlyplayable === true){//add zap information
+			document.getElementById('MMM-Dreambox'+serviceselected).setAttribute('class','selected play');
 			if (onlyplayable === true || this.slp[serviceselected].e2isplayable === "True"){//add zap information
 				payload = [this.sender[parseInt(serviceselected)].e2servicereference,''];
 			} else {// zap before streaming required
@@ -222,6 +183,7 @@ Module.register('MMM-Dreambox', {
 		}
 		
 		if(notification === "DB-STOP"){
+			document.getElementById('MMM-Dreambox'+serviceselected).setAttribute('class','selected');
 			this.sendSocketNotification('DB-STOP', payload);
 			this.sendSocketNotification('FETCH_DATA', payload);
 		}
@@ -232,49 +194,83 @@ Module.register('MMM-Dreambox', {
 				this.startFetchingData(this.config.refreshInterval);
 			} else if (notification === "DATA") {
 				this.loaded = true;
-				if(payload[0]==='DB-EPGNOW'){
-					this.dataRequest = "DB-EPGNOW";
+				if(payload[0]==='DB-EPGNOW'){//no single Dom-Item to refresh, so storing as variable is fine
 					var json=xml2json(payload[1]);
 					this.epg = json.e2eventlist.e2event;
-					this.updateDom();
 				} else if(payload[0]==='DB-ABOUT'){
-					this.dataRequest = "DB-ABOUT";
 					var json=xml2json(payload[1]);
 					this.model = json.e2abouts.e2about.e2model;
 					this.tuned = json.e2abouts.e2about.e2servicename;
-					this.updateDom();
+					document.getElementById('model').innerHTML = this.translate("model") +this.model+ '&nbsp;';
+					if (this.timerstring != undefined){
+						document.getElementById('nowplaying').innerHTML = '- '+ this.timerstring;
+					} else {
+						//Nowplayinginfo.innerHTML = '- ' +this.translate("nowplaying")+ '(' +this.tuned + ')';
+						document.getElementById('nowplaying').innerHTML = '- ' +this.translate("nowplaying")+ '(' +this.tuned + ')';
+					}
 				} else if(payload[0]==='DB-SERVICES'){
-					this.dataRequest = "DB-SERVICES";
 					var json=xml2json(payload[1]);
-					//console.log('Axled Länge Bouquet: '+json.e2servicelistrecursive.e2bouquet.length);
 					//Test if more than one bouquet
-					if (json.e2servicelistrecursive.e2bouquet.length != undefined){//show only first bouquet
+					if (Array.isArray(json.e2servicelistrecursive.e2bouquet) === true){//only one e2bouquet
 						this.sender = json.e2servicelistrecursive.e2bouquet[0].e2servicelist.e2service;
 						this.anzahl = json.e2servicelistrecursive.e2bouquet[0].e2servicelist.e2service.length;
-					} else {
+					} else {//more than one e2bouquet
 						this.sender = json.e2servicelistrecursive.e2bouquet.e2servicelist.e2service;
 						this.anzahl = json.e2servicelistrecursive.e2bouquet.e2servicelist.e2service.length;
 					}
-					this.updateDom();
+					//Add or update servicelist
+					for (let index = 0; index < this.anzahl; index++) {
+
+						if (document.getElementById('MMM-Dreambox'+index) === null) {//if div not there, create and append it
+							var ServiceItem = document.createElement("div");
+							ServiceItem.setAttribute('class', 'db');
+							ServiceItem.setAttribute('id','MMM-Dreambox'+index);
+						} else {//if there, update the content
+							var ServiceItem = document.getElementById('MMM-Dreambox'+index);
+							ServiceItem.setAttribute('class', 'db');//axled, this is needed for unselect at refresh
+						}
+						
+						if(this.epg == undefined){
+							ServiceItem.innerHTML = this.sender[index].e2servicename;
+						} else {
+							if (this.epg[index].e2eventstart === "None") {//no epg information for this service
+								ServiceItem.innerHTML = this.sender[index].e2servicename+' - ('+this.translate("noepginformation")+')';
+							} else {
+								ServiceItem.innerHTML = this.sender[index].e2servicename +' - ('+moment.unix(this.epg[index].e2eventstart).format('HH:mm')+' '+this.epg[index].e2eventtitle+')';
+							}
+						}
+						
+						if(this.tuned === this.sender[index].e2servicename) {
+							ServiceItem.setAttribute('class', 'selected');
+							serviceselected = index;// remember the selected service for play & zapping
+						}
+
+						//mark playable services (only if timer on single tuner receiver is running)
+						if(this.slp != undefined && this.timerstring != undefined){
+							if(this.slp[index].e2isplayable === "False"){
+								ServiceItem.setAttribute('class', 'inactive');
+								onlyplayable = true;
+							}
+						}
+						
+						if (document.getElementById('MMM-Dreambox'+index) === null) {//if div not there, create and append it
+							document.getElementById('MMM-Dreambox').appendChild(ServiceItem);//ID of parent item
+						} 
+					}
 				} else if(payload[0]==='ERROR'){
-					this.dataRequest = "ERROR";
 					this.Errormessage = payload[1];
-					this.updateDom();
-				} else if(payload[0]==='DB-TIMER'){
-					this.dataRequest = payload[0];
+					document.getElementById('error1').innerHTML = this.Errormessage;
+					document.getElementById('error1').removeAttribute('style');//do make it visible
+					document.getElementById('error2').removeAttribute('style');//do make it visible
+				} else if(payload[0]==='DB-TIMER'){//no single Dom-Item to refresh, so storing as variable is fine
 					var json=xml2json(payload[1]);
 					if (json.e2timerlist.e2timer[0].e2state === "2"){//Timer in List are sorted, so only first entry can (should) run, sometimes the second entry runs
-						//this.timerstring = 'Aufnahme läuft: '+json.e2timerlist.e2timer[0].e2name+' ('+json.e2timerlist.e2timer[0].e2servicename+')';
 						this.timerstring = this.translate("recording")+json.e2timerlist.e2timer[0].e2name+' ('+json.e2timerlist.e2timer[0].e2servicename+')';
-						this.updateDom();
 					} 
-				} else if(payload[0]==='DB-SLP'){
-					this.dataRequest = payload[0];
+				} else if(payload[0]==='DB-SLP'){//no single Dom-Item to refresh, so storing as variable is fine
 					var json=xml2json(payload[1]);
 					this.slp = json.e2servicelistplayable.e2serviceplayable;
-					this.updateDom();
 				} 
-				//this.updateDom();
 			}
 	},
 	
@@ -283,7 +279,7 @@ Module.register('MMM-Dreambox', {
 			// ... and then repeat in the given interval
 			IntervalID = setInterval(() => {
 			this.sendSocketNotification("FETCH_DATA", '');
-			}, interval);
+			}, interval); 
 		}
 	}
 
